4491ffb5a91d0342cee396a7ae5e01e94e9e28f5
Ilirjan Rexho <61479363+ilrexho2011@users.noreply.github.com>
2022-12-30 10:49:26 +0100

    Update TERMAT E FIBONACIT DERI TEK TERMI I N-TE C++.cpp

--- TERMAT E FIBONACIT DERI TEK TERMI I N-TE C++.cpp
+++ TERMAT E FIBONACIT DERI TEK TERMI I N-TE C++.cpp
@@ -7,17 +7,17 @@ using namespace std;
  
 int main() 
 {  
-int f0=0,f1=1,f,i,nrterm;    
+int f0=0, f1=1, f, i, nrterm;    
 cout << "Jepni numrin e elementeve: ";    
 cin >> nrterm;
     
  cout << f0 << " " << f1 << " ";  // afishohen 0 dhe 1    
  for(i=2; i<nrterm; i++)          // unaza starton nga 2 sepse 0 dhe 1 jane afishuar    
 	 {    
-		  f=f0+f1;    
+		  f = f0 + f1;    
 		  cout << f << " ";    
-		  f0=f1;    
-		  f1=f;    
+		  f0 = f1;    
+		  f1 = f;    
 	 } 
 	    
 return 0;  
