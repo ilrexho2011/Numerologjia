8ddf316f6722453fa5775f2757bd479b6365fe0d
Ilirjan Rexho <61479363+ilrexho2011@users.noreply.github.com>
2022-12-30 11:26:17 +0100

    Update KONVERTIM NUMRI BINAR NE DECIMAL C++.cpp

--- KONVERTIM NUMRI BINAR NE DECIMAL C++.cpp
+++ KONVERTIM NUMRI BINAR NE DECIMAL C++.cpp
@@ -6,16 +6,16 @@
 using namespace std;
 int main()
 {
-	int i=0,dec=0,bin,mb;
+	int i=0, dec=0, bin, mb;
 	cout << "Jepni numrin ne sistemin binar:" << endl;
 	cout << "bin=";
 	cin  >> bin;
 	while(bin>0)
 	{
-		mb=bin%10;
-	 	bin=bin/10;
-	 	dec=dec+(mb*pow(2,i));
-	 	i=i+1;
+		mb = bin % 10;
+	 	bin = bin / 10;
+	 	dec = dec + (mb * pow(2,i));
+	 	i = i + 1;
 	}
 	cout << "Numri ne sistemin decimal eshte dec=" << dec;
 	return 0;
